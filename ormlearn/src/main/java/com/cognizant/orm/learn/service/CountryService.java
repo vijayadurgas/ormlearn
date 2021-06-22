990efcfa78a491352f216898b2ee7cd7a56211b2
vijayadurgas <86121793+vijayadurgas@users.noreply.github.com>
2021-06-22 22:07:31 +0530

    Update CountryService.java

--- ormlearn/src/main/java/com/cognizant/orm/learn/service/CountryService.java
+++ ormlearn/src/main/java/com/cognizant/orm/learn/service/CountryService.java
@@ -8,6 +8,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.cognizant.orm.learn.model.Country;
 import com.cognizant.orm.learn.repository.CountryRepository;
+import com.cognizant.springlearn.service.exception.CountryNotFoundException;
 
 @Service
 public class CountryService {
@@ -19,5 +20,50 @@ public class CountryService {
 	public List<Country> getAllCountries() {
 		return countryRepository.findAll();
 	}
+	
+	@Transactional
+	public List<Country> findCountry(String text) 
+	{
+		return countryRepository.findCountryBy(text);
+
+	}
+	
+	@Transactional
+	public Country findCountryByCode(String countryCode) throws CountryNotFoundException 
+	{
+		Optional<Country> result = countryRepository.findById(countryCode);
+		if (!result.isPresent()) 
+		{
+			throw new CountryNotFoundException("Country not found");
+		}
+		Country country = result.get();
+		return country;
+	}
+	
+	@Transactional
+	public void addCountry(Country country) 
+	{
+		countryRepository.save(country);
+	}
+	
+	@Transactional
+	public void deleteCountry(String code) 
+	{
+		
+		Country country = (Country) countryRepository.findById(code).get();
+		countryRepository.delete(country);
+	}
+
+	@Transactional
+	public void updateCountry(String code, String name) throws CountryNotFoundException 
+	{
+		
+		Country country = (Country) countryRepository.findById(code).get();
+		country.setName(name);
+		countryRepository.save(country);
+
+	}
+	
+
 
 }
