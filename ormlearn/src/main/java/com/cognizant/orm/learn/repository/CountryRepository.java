f10a625f6c12445f42c16e715d07b661a839564a
vijayadurgas <86121793+vijayadurgas@users.noreply.github.com>
2021-06-22 21:53:23 +0530

    Update CountryRepository.java

--- ormlearn/src/main/java/com/cognizant/orm/learn/repository/CountryRepository.java
+++ ormlearn/src/main/java/com/cognizant/orm/learn/repository/CountryRepository.java
@@ -1,11 +1,16 @@
 package com.cognizant.orm.learn.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
+import org.springframework.data.jpa.repository.Query;
 import com.cognizant.orm.learn.model.Country;
 
 @Repository
 public interface CountryRepository extends JpaRepository<Country, String> {
+  
+  @Query("select c from country c where c.name like %?1% order by name")
+  public List<Country> findByCountryWith(String text);
 
 }
