5ce8fa9254add9e94dfc2c445097d9ee220e3705
vijayadurgas <86121793+vijayadurgas@users.noreply.github.com>
2021-06-28 09:43:32 +0530

    Update OrmLearnApplication.java

--- ormlearn/src/main/java/com/cognizant/orm/learn/OrmLearnApplication.java
+++ ormlearn/src/main/java/com/cognizant/orm/learn/OrmLearnApplication.java
@@ -1,5 +1,7 @@
 package com.cognizant.ormlearn;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -7,6 +9,13 @@ import org.springframework.context.ApplicationContext;
 import com.cognizant.orm.learn.model.Country;
 import com.cognizant.orm.learn.service.CountryService;
 import com.cognizant.springlearn.service.exception.CountryNotFoundException; 
+
+import com.cognizant.ormlearnformapping.model.Department;
+import com.cognizant.ormlearnformapping.model.Employee;
+import com.cognizant.ormlearnformapping.model.Skill;
+import com.cognizant.ormlearnformapping.services.DepartmentService;
+import com.cognizant.ormlearnformapping.services.EmployeeService;
+import com.cognizant.ormlearnformapping.services.SkillService;
 
 import java.util.List;
 
@@ -18,6 +27,9 @@ public class OrmLearnApplication {
   
 	private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
 	private static CountryService countryService;
+	private static EmployeeService employeeService;
+	private static DepartmentService departmentService;
+	private static SkillService skillService;
 
 	private static void testGetAllCountries() {
 
@@ -68,11 +80,95 @@ public class OrmLearnApplication {
 		LOGGER.debug("Country:{}", country);
 		LOGGER.info("End find");
 	}
+	
+	private static void testGetEmployee() {
+
+		LOGGER.info("Start GetEmployee");
+		Employee employee = employeeService.getById(1);
+		LOGGER.debug("Employee:{}", employee);
+		LOGGER.debug("Department:{}", employee.getDepartment());
+		LOGGER.debug("Skills:{}", employee.getSkillList());
+		LOGGER.info("End GetEmployee");
+
+	}
+	
+	private static void testAddEmployee() {
+		
+		LOGGER.info("Start AddEmployee");
+		Department department = departmentService.getById(1);
+		Employee employee = Employee.builder().name("Vijayadurga").salary(BigDecimal.valueOf(40000)).permanent(true)
+				.dateOfBirth(new Date(2021, 09, 20)).department(department).build();
+		employeeService.save(employee);
+		LOGGER.info("End AddEmployee");
+	}
+
+	private static void testUpdateEmployee() {
+
+		LOGGER.info("Start UpdateEmployee");
+		Employee employee = employeeService.getById(1);
+		Department department = departmentService.getById(2);
+		employee.setDepartment(department);
+		employeeService.save(employee);
+		LOGGER.info("End UpdateEmployee");
+	}
+
+	private static void testGetDepartment() {
+		
+		LOGGER.info("Start GetDepartment");
+		Department department = departmentService.getById(3);
+		department.getEmployeeList().forEach(emp -> System.out.println(emp));
+		LOGGER.info("End GetDepartment");
+	}
+
+	private static void testAddSkillToEmployee() {
+		
+		LOGGER.info("Start AddSkill");
+		Employee employee = employeeService.getById(1);
+		Skill skill = skillService.getById(2);
+		employee.getSkillList().add(skill);
+		employeeService.save(employee);
+		LOGGER.info("End AddSkill");
+	}
+
+	private static void getAllPermanent() {
+		
+		LOGGER.info("Start AllPermanent");
+		List<Employee> all = employeeService.getAll();
+		LOGGER.debug("{}", all);
+		LOGGER.info("End AllPermanent");
+	}
+
+	private static void getAverageSalary() {
+		
+		LOGGER.info("Start AverageSalary");
+		double averageSalary = employeeService.getAverageSalary();
+		LOGGER.debug("{}", averageSalary);
+		LOGGER.info("End AverageSalary");
+	}
+
+	private static void getAverageSalaryByDept() {
+		
+		LOGGER.info("Start AverageSalary");
+		double averageSalary = employeeService.getAverageSalaryByDept(3);
+		LOGGER.debug("{}", averageSalary);
+		LOGGER.info("End AverageSalary");
+	}
+	
+	private static void usingNative(){
+		
+		LOGGER.info("Start usingNative");
+		List<Employee> allEmployeesNative = employeeService.getAllEmployeesNative();
+		LOGGER.debug("{}", allEmployeesNative);
+		LOGGER.info("End usingNative");
+	}
 
 	public static void main(String[] args) throws CountryNotFoundException {
     
     		ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
 		countryService = context.getBean(CountryService.class);
+		employeeService = context.getBean(EmployeeService.class);
+		departmentService = context.getBean(DepartmentService.class);
+		skillService = context.getBean(SkillService.class);
 		LOGGER.info("Inside main");
 		testGetAllCountries();
 		getAllCountriesTest("IN");
@@ -81,6 +177,15 @@ public class OrmLearnApplication {
 		testUpdateCountry("SI", "SOUTH INDIA");
     		testFindCountry("SI");
 		testDeleteCountry("SI");
+		// testGetEmployee();
+		// testAddEmployee();
+		// testUpdateEmployee();
+		// testGetDepartment();
+		// testAddSkillToEmployee();
+		// getAllPermanent();
+		// getAverageSalary();
+		// getAverageSalaryByDept();
+		usingNative();
 
 
 	}
