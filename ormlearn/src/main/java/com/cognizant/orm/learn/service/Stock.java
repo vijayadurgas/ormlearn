c70f2591ecc62adfeec18ed52b5e1d685fc08330
vijayadurgas <86121793+vijayadurgas@users.noreply.github.com>
2021-06-29 10:05:04 +0530

    Create

--- ormlearn/src/main/java/com/cognizant/orm/learn/service/Stock.java
+++ ormlearn/src/main/java/com/cognizant/orm/learn/service/Stock.java
@@ -12,6 +12,12 @@ public interface StockService {
 
 	List<Stock> findStockUsingCode(String code);
 
-	List<Stock> findFBStockInSep19(String code, Date startDate, Date endDate);
+	List<Stock> findFBStockInSep21(String code, Date startDate, Date endDate);
+	
+	List<Stock> findGoogleStockGreaterThan1250(String code, double price);
+
+	List<Stock> findTop3VolumeStock();
+
+	List<Stock> findLowest3NetflixStocks(String code);
   
 }
